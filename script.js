8ebf74c88c07f645d6b13d8d238c0526c68ebc55
sebastian chara <sebas7240@gmail.com>
2023-11-02 20:27:16 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+//alert("Hola este es mi Javascript");
+
+//let contenidoTitulo = "nombre";
+
+//let titulo = document.querySelector(".logo .fuente-acentotitulo.innerHTML = contenidoTitulo"
+//);
+
+//condicionales
+///let textoTitulo = titulo.innerText;console, log//(textoTitulo);
+
+//let nombre = "sebastian";
+//console.log(nombre);
+
+//let num1 = 0;
+//let num2 = 20;
+
+//if (num1 == num2) {
+//si se cumple
+// console.log("correcto");
+//} else {
+//de lo contrario
+//console.log("incorrecto");
+//}
+
+//let logo = document.getElementById("primer-button");
+//logo.innerHTML = "soy carlos";
+
+//FUNCIONES
+
+//let nombre;
+
+//function alertar(texto) {
+// alert(texto);
+//}
+//console.log("alert");
+
+//let nombre = "sebastian";
+//------------------------------------------//
+
+//FUNCIONES
+//let nombre = "ani";
+let ciudad = "bs as";
+let gusto = "chocolate";
+
+let parrafo = document.querySelector(".parrafo2");
+
+function cambiarTexto(nombre, ciudad, gusto) {
+  let contenido = `Me llamo ${nombre} sebastian chara banguero, naci en ${cali} colombia y vivo en padilla cauca. Soy amante al ${tecnologia} las criptomonedas y la inteligencia artificial, espero que te sumerjas en un mar de conocimientos y te diviertas aprendiendo.`;
+
+  return contenido;
+}
+
+//parrafo.innerText = cambiarTexto(nombre,ciudad,gusto);
